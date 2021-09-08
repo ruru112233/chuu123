06221483fe32b68fefe6685009b68d336f0ae498
ruru112233 <esxwaz1234@gmail.com>
2021-09-08 23:46:31 +0900

    鳥用のオブジェクトプールを追加

---
+++ Assets/Scripts/InstanceManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceManager : MonoBehaviour
+{
+    public GameObject birdPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("BirdInstance", 0.0f, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // íπÇÃê∂ê¨
+    void BirdInstance()
+    {
+        foreach (Transform t in birdPool.transform)
+        {
+            if (!t.gameObject.activeSelf)
+            {
+                t.SetPositionAndRotation(InstancePoint(), Quaternion.identity);
+                t.gameObject.SetActive(true);
+                return;
+            }
+        }
+
+        GameObject obj = birdPool.transform.GetChild(0).gameObject;
+        Instantiate(obj, InstancePoint(), Quaternion.identity, birdPool.transform);
+    }
+
+    Vector3 InstancePoint()
+    {
+        Vector3 pos = new Vector3();
+
+        pos = new Vector3(0, 0.2f, -5);
+
+
+        return pos;
+    }
+
+}
